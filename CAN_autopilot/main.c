c2756002d5b117fe43d3f8d9d4d6fd0769f6dce0
Mohibiriki <154254351+Mohibiriki@users.noreply.github.com>
2023-12-18 17:39:42 +0100

    Add files via upload

---
+++ CAN_autopilot/main.c
@@ -0,0 +1,261 @@
+/* *************************************************
+
+Basic MCP2515 CAN application. Rx and Tx of CAN frames
+ 
+(c) dkroeske@gmail.com
+
+v1.0    2023-10-10: Initial code
+v1.1    2023-12-04: Added filtering example
+
+***************************************************/
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/spi.h"
+#include "mcp2515.h"
+#include "mcp2515_regs.h"
+
+// Function prototypes
+//
+// CAN callbacks
+void on_can_rx(CAN_DATA_FRAME_STRUCT *frame);
+void on_can_tx(CAN_DATA_FRAME_STRUCT *frame);
+void on_can_err(CAN_ERR_FRAME_STRUCT *err);
+//
+// CAN frame/err debug printf's
+void debug_config();
+void debug_dataframe(CAN_DATA_FRAME_STRUCT *frame);
+void debug_errframe(CAN_ERR_FRAME_STRUCT *frame); 
+
+// 
+#ifndef PICO_STDIO_USB_CONNECT_WAIT_TIMEOUT_MS
+#define PICO_STDIO_USB_CONNECT_WAIT_TIMEOUT_MS (2000)
+#endif
+
+// Main loop
+int main() {
+
+    // Explicit stdio_usb_init() or first printf are skipped
+    stdio_usb_init();
+    sleep_ms(PICO_STDIO_USB_CONNECT_WAIT_TIMEOUT_MS);
+        
+    // set mode { bijvoorbeeld: REQOP_LOOPBACK, REQOP_NORMAL }
+    can_init(REQOP_NORMAL);
+    //can_init(REQOP_LOOPBACK);
+    
+    // Init CAN and set callback handlers
+    can_set_rx_handler(&on_can_rx);
+    can_set_tx_handler(&on_can_tx); // Not working yet, work in progress
+    can_set_err_handler(&on_can_err); // Not working yet, work in progress
+
+    //
+    CAN_DATA_FRAME_STRUCT tx_frame;
+    
+ //   uint16_t x = 0;
+    
+    while (true) {
+
+		
+		
+	 for(uint16_t i=0;i<=5; i++){
+		// Construct 2 CAN data frames, we will filter one out later ...
+		// Frame 1
+		tx_frame.id = 3000;
+		tx_frame.datalen = 1;
+	  	tx_frame.data[0] = 10;
+		// ... and sent it out        
+		if( can_tx_extended_data_frame(&tx_frame) ) {
+		    printf("can_tx_extended_data_frame() ERROR!\n");
+		}
+		sleep_ms(1500);
+		}
+	for(uint16_t i=0;i<=5; i++){
+		// Construct 2 CAN data frames, we will filter one out later ...
+		// Frame 1
+		tx_frame.id = 3000;
+		tx_frame.datalen = 1;
+	  	tx_frame.data[0] = 20;
+		// ... and sent it out        
+		if( can_tx_extended_data_frame(&tx_frame) ) {
+		    printf("can_tx_extended_data_frame() ERROR!\n");
+		}
+		sleep_ms(1500);
+		}
+	for(uint16_t i=0;i<=5; i++){
+		// Construct 2 CAN data frames, we will filter one out later ...
+		// Frame 1
+		tx_frame.id = 3000;
+		tx_frame.datalen = 1;
+	  	tx_frame.data[0] = 30;
+		// ... and sent it out        
+		if( can_tx_extended_data_frame(&tx_frame) ) {
+		    printf("can_tx_extended_data_frame() ERROR!\n");
+		}
+		sleep_ms(1500);
+		}
+	for(uint16_t i=0;i<=5; i++){
+		// Construct 2 CAN data frames, we will filter one out later ...
+		// Frame 1
+		tx_frame.id = 3000;
+		tx_frame.datalen = 1;
+	  	tx_frame.data[0] = 40;
+		// ... and sent it out        
+		if( can_tx_extended_data_frame(&tx_frame) ) {
+		    printf("can_tx_extended_data_frame() ERROR!\n");
+		}
+		sleep_ms(1500);
+		}
+       /*
+        // Construct 2 CAN data frames, we will filter one out later ...
+        // Frame 2
+        tx_frame.id = 0x60;
+        tx_frame.datalen = 2;
+        tx_frame.data[0] = (uint8_t) x;
+        tx_frame.data[1] = (uint8_t) (x>>8);
+        x++;
+  
+        // ... and sent it out        
+        if( can_tx_extended_data_frame(&tx_frame) ) {
+            printf("can_tx_extended_data_frame() ERROR!\n");
+        }
+        sleep_ms(300);
+	*/
+    }
+}
+
+
+/* CAN CALLBACK FUNCTIONS */
+
+/* ************************************************************* */
+void on_can_rx(CAN_DATA_FRAME_STRUCT *frame) 
+/* 
+short   :         
+inputs  :        
+outputs : 
+notes   :         
+Version : DMK, Initial code
+***************************************************************** */
+{
+    debug_dataframe(frame);
+    
+// Filter in code OR in MASK en FILTERS in mcp2515
+//    if( frame->id == 0x101 ) {
+//        debug_dataframe(frame);    
+//    }
+}
+
+/* ***************************************************************************************** */
+void on_can_tx(CAN_DATA_FRAME_STRUCT *frame) 
+/* 
+short   :         
+inputs  :        
+outputs : 
+notes   :         
+Version : DMK, Initial code
+***************************************************************** */
+{
+    puts(">> can_tx()");
+    puts("<< can_tx()");
+}
+
+/* ***************************************************************************************** */
+void on_can_err(CAN_ERR_FRAME_STRUCT *err) 
+/* 
+short   :         
+inputs  :        
+outputs : 
+notes   :         
+Version : DMK, Initial code
+***************************************************************** */
+{
+    puts(">> on_can_err()");
+    debug_errframe(err);
+    puts("<< on_can_err()");
+}
+
+/* UTIL FUNCTIONS */
+
+/* ************************************************************* */
+void debug_config() 
+/* 
+short   :         
+inputs  :        
+outputs : 
+notes   :         
+Version : DMK, Initial code
+***************************************************************** */
+{
+    // 
+    uint8_t data =  mcp2515_read_register(CANSTAT);
+    switch (data >> 5) {
+        case 0x00: 
+            printf("Normal Operating mode\n");
+            break;
+        case 0x01: 
+            printf("Sleep mode\n");
+            break;
+        case 0x02: 
+            printf("Loopback mode\n");
+            break;
+        case 0x03: 
+            printf("Listen-Only mode\n");
+            break;
+        case 0x04: 
+            printf("Configuration mode\n");
+            break;
+        default: 
+            printf("Invalid mode!\n");
+    }
+}
+
+/* ***************************************************************************************** */
+void debug_dataframe(CAN_DATA_FRAME_STRUCT *frame) 
+/* 
+short   : Print CAN dataframe to terminal
+inputs  :        
+outputs : 
+notes   :         
+Version : DMK, Initial code
+***************************************************************** */
+{
+    char buf[1048] = "";
+    char line[80];
+    sprintf(line, "[DATAFRAME] ");
+    strcat(buf, line);
+
+    sprintf(line, "ID: 0x%.8X ", (unsigned int)frame->id);
+    strcat(buf, line);
+    
+    sprintf(line, "LEN: %.2d ", (unsigned int)frame->datalen);
+    strcat(buf, line);
+
+    sprintf(line, "DATA: ");
+    strcat(buf,line);
+    for(uint8_t idx = 0; idx < frame->datalen; idx++) {
+        sprintf(line, "0x%.2X ", frame->data[idx]);
+        strcat(buf, line);
+    }
+    //strcat(buf, "\n");
+    
+    puts(buf);
+}
+
+/* ***************************************************************************************** */
+void debug_errframe(CAN_ERR_FRAME_STRUCT *frame) 
+/* 
+short   : Print CAN errors to terminal
+inputs  :        
+outputs : 
+notes   :         
+Version : DMK, Initial code
+***************************************************************** */
+{
+    printf("*********** MCP 2515 ERROR *************\n");
+    printf("REC    : 0x%.2X\n", frame->rREC);
+    printf("TEC    : 0x%.2X\n", frame->rTEC);
+    printf("EFLG   : 0x%.2X\n", frame->rEFLG);
+    printf("CANINTF: 0x%.2X\n", frame->rCANINTF);
+}
